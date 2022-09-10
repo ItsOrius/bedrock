0faec46bcd1778f992262f9fc3c69c7753d235d0
Orius <32719997+ItsOrius@users.noreply.github.com>
2022-09-10 23:25:30 +0000

    added discord stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// express
 const express = require("express");
 const app = express();
 
@@ -14,3 +15,19 @@ Object.entries(redirects).forEach(obj => {
 app.listen(3000, () => {
   console.log('API started!');
 });
+
+
+
+
+
+// discord.js
+const { Client, Collection, Intents } = require('discord.js');
+
+const client = new Client({ 
+  intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
+  partials: ['MESSAGE']
+});
+
+client.once('ready', () => { console.log(`${client.user.tag} is online!`) })
+
+client.login(process.env.TOKEN);
