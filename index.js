18c49c6f4c10048052f1e591141be15d46ed4155
ItsOrius <thegameradmin@icloud.com>
2022-09-10 19:49:45 -0400

    added discord support

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path")
+
 // express
 const express = require("express");
 const app = express();
@@ -21,11 +25,32 @@ app.listen(3000, () => {
 
 
 // discord.js
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({ 
-  intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
-  partials: ['MESSAGE']
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages],
+  partials: [Partials.Message]
+});
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+  try {
+    await command.execute(client, interaction);
+  } catch (error) {
+    await interaction.reply({ content: `**There was an error while executing this command!**\`\`\`${error}\`\`\``, ephemeral: true });
+  }
 });
 
 client.once('ready', () => { console.log(`${client.user.tag} is online!`) })
