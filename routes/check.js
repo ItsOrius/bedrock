27846e13d3712f56fee77b27c1850772e502570d
ItsOrius <thegameradmin@icloud.com>
2022-09-18 14:03:57 -0400

    added support for multiple api keys

--- routes/check.js
+++ routes/check.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  if (req.headers.authorization != process.env["SECRET"]) return res.status(401).json({ error: "Unauthorized" });
+  if (!JSON.parse(process.env["SECRETS"]).includes(req.headers.authorization)) return res.status(401).json({ error: "Unauthorized" });
   res.status(200).json({ message: "Authorized" });
 });
 
--- routes/submit.js
+++ routes/submit.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.post("/", (req, res) => {
-  if (req.headers.authorization != process.env["SECRET"]) return res.status(401).json({ error: "Unauthorized" });
+  if (!JSON.parse(process.env["SECRETS"]).includes(req.headers.authorization)) return res.status(401).json({ error: "Unauthorized" });
   const { Username, Xuid, Skin, ServerAddress, Platform } = req.body;
   if (!Username || !Xuid || !Skin || !ServerAddress || !Platform) return res.status(400).json({ error: "Bad Request" });
   res.status(200).json({ message: "Success" });
