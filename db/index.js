7df9bde65678bc10bc0c0db7490e28fa1f9ef9be
ItsOrius <thegameradmin@icloud.com>
2023-02-05 14:45:29 -0500

    fixed wrong sqlite & added skinupdates

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize.Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect: 'sqlite',
-  storage: path.join(__dirname, "../sqlite.db")
+  storage: path.join(__dirname, "../db.sqlite")
 });
 
 // setup models
@@ -16,10 +16,6 @@ const BedrockPlayers = db.define('bedrock_players', {
     unique: true
   },
   displayName: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  skinHash: {
     type: Sequelize.TEXT,
     allowNull: false
   },
@@ -53,6 +49,36 @@ const BedrockCapes = db.define('bedrock_capes', {
     defaultValue: false
   }
 });
+const BedrockSkinUpdates = db.define('bedrock_skin_updates', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    unique: true,
+    autoIncrement: true
+  },
+  timestamp: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: Date.now()
+  },
+  xuid: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  hash: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  server: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  token: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+});
 
 /**
  * Returns information about a cape from Minecraft: Bedrock Edition
@@ -82,7 +108,11 @@ async function GetBedrockCapes(query) {
  * @returns {object}
  */
 async function GetBedrockPlayer(query) {
-  return await BedrockPlayers.findOne({ where: query });
+  const player = await BedrockPlayers.findOne({ where: query });
+  if (!player) return null;
+  const skinUpdates = await BedrockSkinUpdates.findAll({ where: { xuid: player.xuid } });
+  player.dataValues.skinUpdates = skinUpdates;
+  return player;
 }
 
 /**
@@ -91,7 +121,13 @@ async function GetBedrockPlayer(query) {
  * @returns {object[]}
  */
 async function GetBedrockPlayers(query) {
-  return await BedrockPlayers.findAll({ where: query });
+  const players = await BedrockPlayers.findAll({ where: query });
+  if (!players) return null;
+  for (const player of players) {
+    const lastSkinUpdate = await BedrockSkinUpdates.findOne({ where: { xuid: player.xuid }, order: [["timestamp", "DESC"]] });
+    player.dataValues.lastSkinUpdate = lastSkinUpdate;
+  }
+  return players;
 }
 
 db.sync();
